34977b0453033c3f7be2d823fbdd7d59b617b24b
Prakash Velusamy <prakash@Alpha-Veer.local>
2022-04-27 23:18:35 +0530

    Terraform-EKS

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
   region  = "ap-south-1"
-  profile = "default"
+  profile = "alphav"
 }
+
 data "aws_availability_zones" "az" {
   state = "available"
 }
@@ -42,14 +43,15 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_eip" "eip" {
-  vpc = true
+  count = var.nat_count ? 1 : 0
+  # subnet = aws_subnet.private ? 1 : 0
   tags = {
     Name = "prakash"
   }
 }
 resource "aws_nat_gateway" "nat" {
   count         = var.nat_count
-  allocation_id = aws_eip.eip.id
+  allocation_id = aws_eip.eip[count.index]
   subnet_id     = aws_subnet.public[count.index].id
 }
 
@@ -76,6 +78,7 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 resource "aws_route_table" "private" {
+  count  = var.priv_sub_count ? 1 : 0
   vpc_id = aws_vpc.main.id
   tags = {
     Name = "Private-route"
@@ -83,19 +86,23 @@ resource "aws_route_table" "private" {
 }
 resource "aws_route" "private" {
   count                  = var.nat_count
-  route_table_id         = aws_route_table.private.id
+  route_table_id         = aws_route_table.private[count.index].id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_nat_gateway.nat[count.index].id
 }
 resource "aws_route_table_association" "private" {
-  count          = var.pub_sub_count
+  count          = var.priv_sub_count ? 1 : 0
   subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.private[count.index].id
 }
 data "aws_security_group" "sg" {
   filter {
     name   = "vpc-id"
     values = [aws_vpc.main.id]
+  }
+  filter {
+    name   = "group-name"
+    values = ["default"]
   }
 }
 resource "aws_instance" "public" {
@@ -122,3 +129,123 @@ resource "aws_instance" "private" {
     Name = var.priv_instancename
   }
 }
+# data "aws_subnet_ids" "public" {
+#   count  = var.pub_sub_count
+#   vpc_id = aws_vpc.main.id
+#   tags = {
+#     Name = "public-subnet_${count.index}"
+#   }
+# }
+# output "aws_subnet_ids" {
+#   value = data.aws_subnet_ids.public
+# }
+
+# terraform {
+#   required_providers {
+#     grafana = {
+#       source  = "grafana/grafana"
+#       version = "1.6.0"
+#     }
+#   }
+# }
+# provider "grafana" {
+#   url  = "http://ab9769ea21fd24ae69b755c54bc49b6f-227768245.ap-south-1.elb.amazonaws.com"
+#   auth = "eyJrIjoiWDJRYkRlUm02MlZtMlpBUEt6UDV0TFdlSjZqeVZKNnIiLCJuIjoidGVycmFmb3JtIiwiaWQiOjF9"
+# }
+# resource "grafana_dashboard" "metrics" {
+#   config_json = file("Final.json")
+# }
+# # resource "grafana_api_key" "foo" {
+# #   name = "key_foo"
+# #   role = "Viewer"
+# # }
+# #
+# # resource "grafana_api_key" "bar" {
+# #   name            = "key_bar"
+# #   role            = "Admin"
+# #   seconds_to_live = 30
+# # }
+# #
+# #
+# # output "api_key_foo_key_only" {
+# #   value     = grafana_api_key.foo.key
+# #   sensitive = true
+# # }
+# #
+# # output "api_key_bar" {
+# #   value = grafana_api_key.bar
+# # }
+# # resource "grafana_data_source" "influxdb" {
+# #   type          = "influxdb"
+# #   name          = "test_influxdb"
+# #   url           = "http://influxdb.example.net:8086/"
+# #   username      = "foo"
+# #   password      = "bar"
+# #
+# # }
+
+#
+# #
+# # resource "aws_iam_instance_profile" "node_group_instance_profile" {
+# #   name_prefix = "EKSNodeGroupRole-"
+# #   role        = aws_iam_role.node_group_role.name
+# # }
+# #
+# # resource "aws_iam_role" "node_group_role" {
+# #   name_prefix = "EKSNodeGroupRole-"
+# #   path        = "/"
+# #
+# #   assume_role_policy = <<EOF
+# # {
+# #     "Version": "2012-10-17",
+# #     "Statement": [
+# #         {
+# #             "Action": "sts:AssumeRole",
+# #             "Principal": {
+# #                "Service": "ec2.amazonaws.com"
+# #             },
+# #             "Effect": "Allow",
+# #             "Sid": ""
+# #         }
+# #     ]
+# # }
+# # EOF
+# # }
+# # resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+# #   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+# #   role       = aws_iam_role.node_group_role.name
+# # }
+# #
+# # resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+# #   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+# #   role       = aws_iam_role.node_group_role.name
+# # }
+#resource "aws_eks_cluster" "eks" {
+#  name     = "EKS"
+#  role_arn = "arn:aws:iam::522820335540:role/AmazonEKSClusterServiceRole"
+#  version  = null
+#  # endpoint_private_access = var.cluster_endpoint_private_access
+#  # endpoint_public_access  = var.cluster_endpoint_public_access
+#  # public_access_cidrs     = var.cluster_endpoint_public_access_cidrs
+#  vpc_config {
+#    subnet_ids = aws_subnet.public[*].id
+#  }
+#}
+#resource "aws_eks_node_group" "ng" {
+#  cluster_name    = aws_eks_cluster.eks.name
+#  node_group_name = "node_group"
+#  node_role_arn   = "arn:aws:iam::522820335540:role/AmazonEKSNodeRole"
+#  subnet_ids      = aws_subnet.public[*].id
+#  scaling_config {
+#    desired_size = 1
+#    max_size     = 3
+#    min_size     = 1
+#  }
+#  update_config {
+#    max_unavailable = 1
+#  }
+#  # depends_on = [
+#  #   aws_iam_role_policy_attachment.Cluster_Policy
+#  # ]
+#}
+#
--- variables.tf
+++ variables.tf
@@ -18,3 +18,20 @@ variable "pub_instance_type" {
 }
 variable "priv_instance_type" {
 }
+# variable "cluster_endpoint_private_access" {
+#   description = "Indicates whether or not the Amazon EKS private API server endpoint is enabled."
+#   type        = bool
+#   default     = false
+# }
+#
+# variable "cluster_endpoint_public_access" {
+#   description = "Indicates whether or not the Amazon EKS public API server endpoint is enabled. When it's set to `false` ensure to have a proper private access with `cluster_endpoint_private_access = true`."
+#   type        = bool
+#   default     = true
+# }
+#
+# variable "cluster_endpoint_public_access_cidrs" {
+#   description = "List of CIDR blocks which can access the Amazon EKS public API server endpoint."
+#   type        = list(string)
+#   default     = ["0.0.0.0/0"]
+# }
